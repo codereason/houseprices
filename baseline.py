2ef76abf267335cf81d5debd31a5b1443e8ded83
huangyu1996 <sdu_huangyu@foxmail.com>
2018-08-10 11:57:14 +0800

    complete base model

--- baseline.py
+++ baseline.py
@@ -7,6 +7,9 @@ from scipy.stats import norm
 from scipy import stats
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from xgboost import XGBRegressor
+import  xgboost as xgb
+
 
 
 def read_data():
@@ -15,10 +18,9 @@ def read_data():
     return train_df, test_df
 
 
-def feature_engineering(df):
-
-    df = df.drop('id')
+def handle_feature(df):
     df = pd.get_dummies(df)
+    print(' Features shape: ', df.shape)
     df = df.apply(lambda x: x.fillna(x.median()),axis=0)
     return df
 
@@ -31,12 +33,34 @@ if __name__ == "__main__":
 
     train_df,test_df = read_data()
 
-    train_df = feature_engineering(train_df)
-    test_df = feature_engineering(test_df)
+    train_df = handle_feature(train_df)
+    test_df = handle_feature(test_df)
+    train_labels = train_df['SalePrice']
+    train, test = train_df.align(test_df, join='inner', axis=1)
+    X_train = train
+    Y_train = train_labels
+    X_test = test
 
 
 
-    my_submission = pd.DataFrame({'Id': test_df.Id, 'SalePrice': Y_pred})
+
+    SEED = 2018
+    regr1 = xgb.XGBRegressor(
+        colsample_bytree=0.2,
+        gamma=0.0,
+        learning_rate=0.01,  # 0.01
+        max_depth=4,  # 4
+        min_child_weight=2,
+        n_estimators=7200,  # 3000
+        reg_alpha=0.9,
+        reg_lambda=0.6,
+        subsample=0.2,
+        random_state=SEED,
+        silent=True)
+    mod = regr1.fit(X_train, Y_train)
+    Y_pred = mod.predict(X_test)
+
+    my_submission = pd.DataFrame({'Id': test.Id, 'SalePrice': Y_pred})
     my_submission.to_csv('to_submit_%d.csv' % (1), index=False)
 
 
